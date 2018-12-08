2d87a649b043709af9d6e1c8183a7666b899d95b
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-09 00:05:42 +0100

    Repair code with array copy

--- kolokwium2-ako/zad2A.asm
+++ kolokwium2-ako/zad2A.asm
@@ -1,11 +1,12 @@
 .686
 .model flat
 public _kopia_tablicy
-extern _malloc PROC
+extern _malloc: PROC
 
 .data
+rozmiar_liczby	dw 4
+rozmiar_tablicy	dd ?
 .code
-
 _kopia_tablicy PROC
 	push ebp
 	mov ebp, esp
@@ -13,30 +14,39 @@ _kopia_tablicy PROC
 	push edi
 	push ebx
 	push ecx
+	push edx
 
 	mov ecx, [ebp+12]	; n
 	mov esi, [ebp+8]	; tabl
-	push ecx	; argument do malloca
+	mov ax, cx
+	mul rozmiar_liczby	; bo tyle zajmuje jedna liczba
+	mov rozmiar_tablicy, eax	; zapamietanie rozmiary w docelowej tablicy 
+	push eax	; argument do malloca
 	call _malloc
 	add esp, 4
 ; w eax adres nowej tablicy
 	cmp eax, 0
 	jz koniec_programu
-	mov ecx, [ebp+12]	; potrzebne do petli
+	mov ecx, 0	; potrzebne do petli
+	mov edi, eax	; docelowy adres zapisu
 ptl:
 	mov ebx, [esi+4*ecx]	; wartosc z tablicy
 	bt ebx, 0	; sprawdzenie czy parzysta
-	jnc nie_parzysta
+	jc nie_parzysta
 	clc
-	mov [esi], ebx	; zapis do tablicy parzystych
-	add esi, 4	; bo kazda liczba zajmuje 4 bajty
+	mov [edi], ebx	; zapis do tablicy parzystych
+	add edi, 4
 nie_parzysta:
-	dec ecx
+	inc ecx
+	cmp ecx, rozmiar_tablicy
 	jnz ptl
 koniec_programu:
+	pop edx
 	pop ecx
 	pop ebx
 	pop edi
 	pop esi
+	pop ebp
 	ret
 _kopia_tablicy ENDP
+END
