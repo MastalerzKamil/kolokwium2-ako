6d530eb7f9b413b69cf7273fa9119a39614d78ce
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-19 19:54:45 +0100

    Add exercise 4

---
+++ kolokwium2-ako/zad4A.asm
@@ -0,0 +1,45 @@
+.686
+.model flat
+public _szukaj_elem_min
+
+.data
+rozmiar	dd 0
+.code
+_szukaj_elem_min PROC
+	push ebp
+	mov ebp, esp
+	push ebx
+	push esi
+	push edi
+	push ecx
+
+	mov esi, [ebp+8]	; tablica
+	mov ecx, [ebp+12]	; n
+
+	mov rozmiar, ecx
+	mov ecx, 0
+	lea eax, [esi+4*ecx]	; pierwszy element najmniejszy
+iteruj:
+	push ecx ; zapamietaj iterator z zewnetrznej petli
+porownuj:
+	inc ecx
+	cmp ecx, rozmiar	; koniec petli wewnetrznej
+	jz po_porownaniu
+	mov ebx, [esi+4*ecx]
+	cmp [eax], ebx
+	jbe porownuj	; eax >= 0 wiec wieksze
+	lea eax, [esi+4*ecx]
+	jmp porownuj
+po_porownaniu:
+	pop ecx	; przwrocenie iteratora z petli zewnetrznej
+	inc ecx
+	cmp ecx, rozmiar
+	jnz iteruj
+	pop ecx
+	pop edi
+	pop esi
+	pop ebx
+	pop ebp
+	ret
+_szukaj_elem_min ENDP
+END
--- kolokwium2-ako/zad4C.c
+++ kolokwium2-ako/zad4C.c
@@ -1,11 +1,12 @@
-// #include <stdio.h>
-// 
-// int* szukaj_elem_min(int tablica[], int n);
-// 
-// int main()
-// {
-// 	int pomiary[7], *wsk;
-// 	wsk = szukaj_elem_min(pomiary, 7);
-// 	printf("\nElement minimalny = %d\n", *wsk);
-// 	return 0;
-// }
+#include <stdio.h>
+
+int* szukaj_elem_min(int tablica[], int n);
+
+int main()
+{
+	int pomiary[7] = {6,7,4,0,2,1,1};
+	int* wsk;
+	wsk = szukaj_elem_min(pomiary, 7);
+	printf("\nElement minimalny = %d\n", *wsk);
+	return 0;
+}
