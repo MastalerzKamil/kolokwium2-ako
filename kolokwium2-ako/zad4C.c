4f3368863abb455b6a549e0ec109684ce3745880
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-19 19:37:42 +0100

    Add C code for exercise 4

---
+++ kolokwium2-ako/zad4C.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// 
+// int* szukaj_elem_min(int tablica[], int n);
+// 
+// int main()
+// {
+// 	int pomiary[7], *wsk;
+// 	wsk = szukaj_elem_min(pomiary, 7);
+// 	printf("\nElement minimalny = %d\n", *wsk);
+// 	return 0;
+// }
