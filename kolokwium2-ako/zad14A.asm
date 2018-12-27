7f729b33e88fa8d66c3f60b8771dce7820201701
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2018-12-28 00:53:35 +0100

    Repair conversion in  exercise 14

--- kolokwium2-ako/zad14A.asm
+++ kolokwium2-ako/zad14A.asm
@@ -5,13 +5,14 @@ extern _ExitProcess@4 : PROC
 
 .data
 test_float			dd 12.25
-mantysa				dq 0
+mantysa				dd 0
 double_wykladnik	dw 0
 znak				dw 0
 liczba_double		dw 4 dup (?)
 .code
 _main PROC
 	mov esi, offset test_float
+	mov edi, offset liczba_double
 	mov edx, [esi]
 	bt edx, 31
 	jnc dodatnia
@@ -23,25 +24,20 @@ dodatnia:
 	sub eax, 127	; wyciagniecie przesuniecia
 	add eax, 1023	; stworz wykladnik double
 	mov double_wykladnik, ax
-; tworzenie mantysy
+; tworzenie mantysy i zapisanie mantysy
+	mov [edi], word ptr 0	; castowanie do double powoduje dodanie zer na najmlodszych 2 najmlodszych bajtach
 	mov eax, [esi]
 	shl eax, 9	; wymazanie wykladnika i znaku z zrodlowej liczby
-	mov dword PTR mantysa, eax	; zapisz mantyse ktora sie nie zmienia
-	mov eax, 0
+	;shr eax, 9	; cofam aby odpowiednio mantyse do pamieci zapisac
+	mov dword PTR mantysa, eax
+	shl eax, 4	; przesun o 4 bity w lewo w celu nie zapisania bitow ktore beda w slowie z wykladnikiem
+	mov [edi+2], eax	; zapisz mantyse ktora sie nie zmienia
+	mov edx, dword PTR mantysa
+	shr edx, 28	; pozbycie sie zapisanych juz bitow w mantysie
 	mov ax,  double_wykladnik
-	shl ax, 4	; zostaw miejesce na znak. Po wyonaniu sytuacja 0xxxxxxxxxxx0000
-	mov bx, word ptr double_wykladnik
-	sal bx, 4	;uzupelnij ax pierwszymi 4 bitami
-	and bx, 000Fh	; maska dla 4 bitow
-	or ax, bx	; zlacz 4 bity i zapisz w ax
-	or ax, znak	; sklej symbol znaku z 0wwwwwwwwwwwmmmm
-	mov edi, offset liczba_double
-	mov [edi], ax	; przenies ax do obszaru pamieci ze skonwertowana liczba
-	mov eax, dword PTR mantysa
-	shl eax, 4	; 4 bity zostaly juz zapisane
-	mov [edi+2], eax	; zapisz pozostale bity mantysy
-	mov eax, 0
-	mov [edi+6], ax	; double zajmuje 64 bity wiec w pozostale miejsca wpisz 0
+	shl ax, 4	; zrobienie miejsca na posostalosci z mantysy
+	or ax, dx	; polaczenie wykladnika i posotalosci mantysy
+	mov [edi+6], ax
 	push 0
 	call _ExitProcess@4
 _main ENDP
